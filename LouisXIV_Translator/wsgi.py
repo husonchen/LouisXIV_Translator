a4a68a2003f6e6e0b3265eab00097cb451e82a41
husonchen <1092441751@qq.com>
2017-01-10 16:57:34 +0800

    init

---
+++ LouisXIV_Translator/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for LouisXIV_Translator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LouisXIV_Translator.settings")
+
+application = get_wsgi_application()
